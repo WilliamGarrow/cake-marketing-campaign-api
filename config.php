24bdab5525258d23516c402d3449ecbfdbff53ef
William Garrow <email@williamgarrow.com>
2017-12-16 21:27:29 -0500

    Added configuration variables file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Example usage
+// Add client specific data here
+$mailTo = 'recipient@example.com';
+$mailFrom = 'data@example.com';
+$subject = 'Your marketing campaign summary data is ready!';
+
+$dataDirectory = 'data';
+$download_loc = 'http://download-loc-example.com/';
+
+// Verbose option on or off via changing the debug variable to true or false.
+$debug = true;
